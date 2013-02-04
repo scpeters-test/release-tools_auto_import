e9cbf8840a179fe5acf59d62b647c3150f8f4df1
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-02-04 10:02:25 +0100

    Back to try a solution based on run the build from the generator

--- jenkins-scripts/gazebo-gazebo_1.4-devel-quantal-amd64.bash
+++ jenkins-scripts/gazebo-gazebo_1.4-devel-quantal-amd64.bash
@@ -4,6 +4,6 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-export BUILD_SH_SCRIPT="$(cat ${SCRIPT_DIR}/build/build-gazebo1.4-quantal.sh)"
+BUILD_SH_SCRIPT="${SCRIPT_DIR}/build/build-gazebo1.4-quantal.sh"
 
 . ${SCRIPT_DIR}/generators/generator-quantal.sh
