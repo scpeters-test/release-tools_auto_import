ab388fa60915c2d2297a89f84724bb17bfc834c5
Steve Peters <scpeters@osrfoundation.org>
2015-03-21 02:22:19 -0700

    Remove --HEAD options to speed up qt installation

--- jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/ign_transport-default-devel-homebrew-amd64.bash
@@ -4,7 +4,5 @@
 [[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-IS_A_HEAD_FORMULA=true
-
 . ${SCRIPT_DIR}/lib/project-default-devel-homebrew-amd64.bash ignition-transport
 
