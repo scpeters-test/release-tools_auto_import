7659ab15d724a4251fe8d8f645e4f55ef4955a93
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-09-18 22:11:18 +0200

    Initial implemente of multiany for handsim

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -302,17 +302,18 @@ IGN_TRANSPORT_DEPENDENCIES="pkg-config           \\
 #
 # HAPTIX
 #
-HAPTIX_COMM_DEPENDENCIES="pkg-config                \\
-                          libignition-transport-dev \\
-                          libboost-system-dev       \\
-			  libprotoc-dev             \\
-			  libprotobuf-dev           \\
-			  protobuf-compiler         \\
+HAPTIX_COMM_DEPENDENCIES_WITHOUT_IGN="pkg-config  \\
+                          libboost-system-dev     \\
+			  libprotoc-dev           \\
+			  libprotobuf-dev         \\
+			  protobuf-compiler       \\
                 	  liboctave-dev"
-
+HAPTIX_COMM_DEPENDENCIES="${HAPTIX_COMM_DEPENDENCIES_WITHOUT_IGN} \\
+                          libignition-transport-dev"
 #
 # HANDSIM
 #
-HANDSIM_DEPENDENCIES="libgazebo7-haptix-dev \\
-                      liboctave-dev \\
+HANDSIM_DEPENDENCIES_WITHOUT_HAPTIX="libgazebo7-haptix-dev \\
+                                     liboctave-dev"
+HANDSIM_DEPENDENCIES="${HANDSIM_DEPENDENCIES_WITHOUT_HAPTIX} \\
                       libhaptix-comm-dev"
