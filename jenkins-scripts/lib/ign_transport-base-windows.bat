0a27dd5e36a001a0dc4228a77532172571baedbb
Steve Peters <scpeters@osrfoundation.org>
2016-03-03 11:23:25 -0800

    add extra echo statements

--- jenkins-scripts/lib/ign_transport-base-windows.bat
+++ jenkins-scripts/lib/ign_transport-base-windows.bat
@@ -32,10 +32,15 @@ call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/cppzmq-noarch.
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/protobuf-2.6.0-win%BITNESS%-vc12.zip protobuf-2.6.0-win%BITNESS%-vc12.zip
 call %win_lib% :wget http://packages.osrfoundation.org/win32/deps/zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip
 
+echo testing 1
 call %win_lib% :download_7za
+echo testing 2
 call %win_lib% :unzip_7za cppzmq-noarch.zip > cppzmq_7z.log || goto :error
+echo testing 3
 call %win_lib% :unzip_7za protobuf-2.6.0-win%BITNESS%-vc12.zip > protobuf_7z.log || goto :error
+echo testing 4
 call %win_lib% :unzip_7za zeromq-3.2.4-%PLATFORM_TO_BUILD%.zip > zeromq_7z.log || goto :error
+echo testing 5
 
 echo # BEGIN SECTION: move sources so we agree with configure.bat layout
 :: Remove code copy
