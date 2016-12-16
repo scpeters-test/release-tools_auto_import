83fa30cd8694ee8139cbed5a6cd2e3365927c5ea
Steve Peters <scpeters@osrfoundation.org>
2016-12-16 15:01:42 -0800

    use ogre1.9 instead of ogre

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -64,7 +64,7 @@ brew install ${HEAD_STR} ${PROJECT} ${PROJECT_ARGS} --only-dependencies
 echo '# END SECTION'
 
 if [[ -n "${BREW_PACKAGES_NEEDED}" ]]; then
-  brew remove qt qwt-qt4
+  brew remove ogre qt qwt-qt4
   brew install ${BREW_PACKAGES_NEEDED}
   export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:/usr/local/opt/qt52
   export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:/usr/local/opt/qt55
