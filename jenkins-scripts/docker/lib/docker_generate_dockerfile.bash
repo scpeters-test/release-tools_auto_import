41f6aef6b69d0a318b0dca6ca7d8e3c98fbc5010
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-05-07 21:58:05 +0200

    Right order

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -175,10 +175,10 @@ if [[ $ARCH == 'arm64' ]]; then
 cat >> Dockerfile << DELIM_SYSCAL_ARM64
 # Workaround for problem with syscall 277 in man-db
 RUN echo 'apt::sandbox::seccomp "false";' > /etc/apt/apt.conf.d/999seccomp
-RUN apt-get update && \\
-    apt-get install -f
 RUN wget https://build.osrfoundation.org/job/generic_backport-debbuilder/40/artifact/pkgs/man-db_2.7.6.1-2%7Eubuntu16.04.1_amd64.deb
 RUN dpkg -i man-db*.deb
+RUN apt-get update && \\
+    apt-get install -f
 DELIM_SYSCAL_ARM64
 fi
 
