f86d5fb2d320aad3842dc953bcd6239ca335bc13
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-05-08 17:12:57 +0200

    Need to install curl

--- jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
+++ jenkins-scripts/docker/lib/docker_generate_dockerfile.bash
@@ -175,7 +175,7 @@ if [[ $ARCH == 'arm64' ]]; then
 cat >> Dockerfile << DELIM_SYSCAL_ARM64
 # Workaround for problem with syscall 277 in man-db
 RUN apt-get update && \\
-    apt-get install -y libseccomp-dev libseccomp2
+    apt-get install -y libseccomp-dev libseccomp2 curl
 RUN curl https://launchpad.net/ubuntu/+source/apt/1.6.1/+build/14780697/+files/libapt-pkg5.0_1.6.1_arm64.deb --output libapt.deb
 RUN dpkg -i libapt.deb
 RUN curl https://launchpad.net/ubuntu/+source/apt/1.6.1/+build/14780697/+files/apt_1.6.1_arm64.deb --output apt.deb
