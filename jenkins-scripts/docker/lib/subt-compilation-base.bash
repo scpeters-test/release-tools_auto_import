710fe375b7ce4cec78d266a3b1409e192bece1d7
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-21 21:03:42 +0200

    Do not display warn about QT (thousands of lines)

--- jenkins-scripts/docker/lib/subt-compilation-base.bash
+++ jenkins-scripts/docker/lib/subt-compilation-base.bash
@@ -40,7 +40,7 @@ source ./install/setup.bash || true
 
 TEST_TIMEOUT=180
 TEST_START=\$(date +%s)
-timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign
+timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign | grep -v '[QT]'
 TEST_END=\$(date +%s)
 DIFF=\$(expr \$TEST_END - \$TEST_START)
 
