b7386f846ddcc0c97dd2e4e4ebe08a50ef0f893e
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-22 12:47:57 +0200

    Filter output

--- jenkins-scripts/docker/lib/subt-compilation-base.bash
+++ jenkins-scripts/docker/lib/subt-compilation-base.bash
@@ -40,7 +40,7 @@ source ./install/setup.bash || true
 
 TEST_TIMEOUT=180
 TEST_START=\$(date +%s)
-timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign | grep -v '[QT]'
+timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign 2> >(grep -v '[QT]')
 TEST_END=\$(date +%s)
 DIFF=\$(expr \$TEST_END - \$TEST_START)
 
