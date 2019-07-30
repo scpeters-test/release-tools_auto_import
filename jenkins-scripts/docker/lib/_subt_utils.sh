e9f10eb9cad8657434ad62b8781076e3e7b9cd8e
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-31 00:11:42 +0200

    Change the testing world

--- jenkins-scripts/docker/lib/_subt_utils.sh
+++ jenkins-scripts/docker/lib/_subt_utils.sh
@@ -6,7 +6,7 @@ fi
 
 TEST_TIMEOUT=\${TEST_TIMEOUT:-180}
 TEST_START=\$(date +%s)
-timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 competition.ign 2>&1 | grep -v -e '[QT]' -e '\"\"'
+timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 tunnel_circuit_practice.ign 2>&1 | grep -v -e '[QT]' -e '\"\"'
 TEST_END=\$(date +%s)
 DIFF=\$(expr \$TEST_END - \$TEST_START)
 
