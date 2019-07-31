421f0d8f74d698534417275d436741ec003831b9
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-07-31 18:34:56 +0200

    Try to kill build in not SIGTERM

--- jenkins-scripts/docker/lib/_subt_utils.sh
+++ jenkins-scripts/docker/lib/_subt_utils.sh
@@ -53,8 +53,9 @@ if [[ -n "${TEST_TIMEOUT}" ]]; then
 fi
 
 TEST_TIMEOUT=\${TEST_TIMEOUT:-180}
+TEST_TIMEOUT_KILL=\$((TEST_TIMEOUT + 30))
 TEST_START=\$(date +%s)
-timeout --preserve-status \$TEST_TIMEOUT ign launch -v 4 tunnel_circuit_practice.ign 2>&1 | grep -v -e '[QT]' -e '\"\"'
+timeout --preserve-status \$TEST_TIMEOUT -k \${TEST_TIMEOUT_KILL} ign launch -v 4 tunnel_circuit_practice.ign 2>&1 | grep -v -e '[QT]' -e '\"\"'
 TEST_END=\$(date +%s)
 DIFF=\$(expr \$TEST_END - \$TEST_START)
 
