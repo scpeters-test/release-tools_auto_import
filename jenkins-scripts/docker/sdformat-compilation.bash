5e77d84cb13226577d645b336eca028cef7fd72a
Steve Peters <scpeters@openrobotics.org>
2019-11-06 15:39:23 -0800

    prereleases for sdformat

--- jenkins-scripts/docker/sdformat-compilation.bash
+++ jenkins-scripts/docker/sdformat-compilation.bash
@@ -17,6 +17,7 @@ fi
 . "${SCRIPT_DIR}/lib/_sdformat_version_hook.bash"
 
 export BUILDING_SOFTWARE_DIRECTORY="sdformat"
+export BUILDING_JOB_REPOSITORIES="stable prerelease"
 
 if [[ ${SDFORMAT_MAJOR_VERSION} -ge 6 ]]; then
   export BUILDING_EXTRA_CMAKE_PARAMS="-DUSE_INTERNAL_URDF:BOOL=True"
